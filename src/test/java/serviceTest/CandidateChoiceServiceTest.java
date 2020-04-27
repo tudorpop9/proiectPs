a35b837ee99c5bd5974bc1e675528f42f89423fd
Tudor Andrei Pop <tudor_fb@yahoo.com>
2020-04-27 23:38:36 +0300

    Am adaugat teste pentru CandidateChoiceService, 6 more to go

--- src/test/java/serviceTest/CandidateChoiceServiceTest.java
+++ src/test/java/serviceTest/CandidateChoiceServiceTest.java
@@ -8,25 +8,21 @@ import enums.CandidatePosition;
 import exception.PersonRequirementsException;
 import exception.WrongObjTypeException;
 import org.junit.Test;
-import org.junit.internal.runners.statements.Fail;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import repository.ChoiceRepo;
 import service.CandidateChoiceService;
 import org.junit.*;
-import start.App;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.fail;
 
@@ -96,5 +92,109 @@ public class CandidateChoiceServiceTest {
         } catch (WrongObjTypeException e) {
             Assert.assertTrue(true);
         }
+    }
+
+    @Test
+    public void getAllCandidateChoicesTest() throws ParseException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        CandidateChoice choice = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        CandidateChoice choice2 = new CandidateChoice("goProtest", new Person(1223L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        CandidateChoice choice3 = new CandidateChoice("obj1", new Person(1231231223L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        List<CandidateChoice> expectedArr = new ArrayList<>();
+        expectedArr.add(choice);
+        expectedArr.add(choice2);
+        expectedArr.add(choice3);
+        Mockito.doReturn(expectedArr).when(repo).findAll(); // interesting syntax
+
+        List<Choice> returned = service.getAllCandidates();
+        Assert.assertEquals(returned, expectedArr);
+
+    }
+
+    @Test
+    public void getCandidateChoiceCorrectType() throws ParseException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        CandidateChoice choice = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(choice);
+
+        try {
+            service.getCandidateChoice("testTitle");
+            Assert.assertTrue(true);
+        } catch (WrongObjTypeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void getCandidateChoiceWrongType() throws ParseException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        Choice choice = new Choice("testTitle");
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(choice);
+
+        try {
+            service.getCandidateChoice("testTitle");
+            fail();
+        } catch (WrongObjTypeException e) {
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void updateCandidateChoiceCorrectType() throws ParseException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        CandidateChoice choice = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(choice);
+
+        try {
+            service.updateCandidateChoice(choice);
+            Assert.assertTrue(true);
+        } catch (WrongObjTypeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void updateCandidateChoiceWrongType() throws ParseException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        Choice choice = new Choice("testTitle");
+        CandidateChoice badChoice = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(choice);
+
+        try {
+            service.updateCandidateChoice(badChoice);
+            fail();
+        } catch (WrongObjTypeException e) {
+            Assert.assertTrue(true);
+        }
+    }
+
+    //Trebuie sa mai testez update-ul iar ?(din cauza exceptiei WrongObjTypeException)
+    @Test
+    public void increaseVotesTest() throws ParseException, WrongObjTypeException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        CandidateChoice toBeSent = new CandidateChoice("testTitle", new Person(1212342343L,"Donald","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        CandidateChoice expected = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(expected);
+
+        expected.incrementVotes();
+        service.increaseVotes(toBeSent);
+        Assert.assertEquals(expected.getVotes(), toBeSent.getVotes());
+    }
+
+    @Test
+    public void addVotesInBulkTest() throws ParseException, WrongObjTypeException {
+        Date bdate = dateFormat.parse("14/04/1975");
+        CandidateChoice toBeSent = new CandidateChoice("testTitle", new Person(1212342343L,"Donald","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        CandidateChoice expected = new CandidateChoice("testTitle", new Person(123L,"Trump","Trump", bdate,"home"),new Party(null,"idk",null,null), CandidatePosition.PRESIDENT);
+        Mockito.when(repo.findByTitle("testTitle")).thenReturn(expected);
+
+        expected.incrementVotes();
+        expected.incrementVotes();
+        expected.incrementVotes();
+        expected.incrementVotes();
+
+        service.addVotesInBulk(toBeSent, 4L);
+        Assert.assertEquals(expected.getVotes(), toBeSent.getVotes());
     }
 }
